a0c19a9b0130b37ea483f3aec5590e376b84353b
KGY <rldyd1205@naver.com>
2021-12-22 00:37:55 +0900

    commit

--- springspring/src/main/java/com/example/controller/MemberController.java
+++ springspring/src/main/java/com/example/controller/MemberController.java
@@ -57,6 +57,17 @@ public class MemberController {
 		// ex) id, passwd ... 제대로 가져왔는지 확인 하기 위해선 출력해보면 된다
 		System.out.println("MemberVO : " + memberVO);
 		
+		// 1.아이디 중복체크(DB에 있는지 확인)
+		// memberVO안에 들어있는 id를 getId로 가지고 옴
+		// DB안에 들어있는 id를 가지고 와서 id에다가 대입
+		// DB에 있는지 확인 하기 위해서 MemberMapper.java에 가서 작성
+		String id = memberVO.getId();
+		
+		// 2.비밀번호, 비밀번호 확인 서로 같은지 체크
+		
+		// 3.DB에 등록
+		
+		// 4.회원가입완료 메세지 띄우고, 로그인 화면으로 이동
 		return null;
 	} //join
 }
--- springspring/src/main/java/com/example/mapper/MemberMapper.java
+++ springspring/src/main/java/com/example/mapper/MemberMapper.java
@@ -1,9 +1,19 @@
 package com.example.mapper;
 
+import com.example.domain.MemberVO;
+
 public interface MemberMapper {
 
 	// =========== select ===========
 	
+	// MemberController에서 회원가입을 하기위해
+	// 중복되지 않은 아이디로 회원가입을 하려고 하기 때문에
+	// 저장되어 있는 아이디가 있는지 확인
+	// 그래서 MemberVO를 사용을 하고 이름은getMemberById로 내가 만들어주고
+	// 가져올건 id밖에 없으니까 괄호 안에 String id라고 적어준다
+	// 그리고 명세를 해줘야 하는데 이게 어떤 sql문을 사용하는지
+	// MemberMapper.xml 파일에서 sql문을 작성을 해줘야 한다.
+	MemberVO getMemberById(String id);
 	// =========== insert ===========
 	
 	// =========== update ===========
